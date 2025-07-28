8d83c9825498986147726fc047d93eb502385344
redraven001 <cdeva405@gmail.com>
2025-07-28 12:41:08 +0530

    day 2

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 const rheading=React.createElement('div',{id:"p"},React.createElement('div',{id:'c'},[React.createElement('h3',{id:"rhdg"},"Welcome to React"),React.createElement('p',{id:"ptxt"},"lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, cumque!")])); 
 console.log(rheading);
 const rootelement=ReactDOM.createRoot(document.getElementById('rRoot'));
